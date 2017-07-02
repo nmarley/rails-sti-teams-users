5651713316e9c9a5cf9fdbbabfed1d2f3abebbbc
Nathan Marley <nathan.marley@gmail.com>
2017-07-01 23:56:13 -0700

    User model tests and field validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < Entity
   has_and_belongs_to_many :teams, join_table: 'teams_users'
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :login, presence: true, uniqueness: { case_sensitive: false }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "should not save user without email" do
+    user = User.new(login: 'nmarley')
+    assert_not user.save, "Saved the user without an email"
+  end
+
+  test "should not save user without login" do
+    user = User.new(email: 'nate@example.com')
+    assert_not user.save, "Saved the user without a login"
   end
 end
