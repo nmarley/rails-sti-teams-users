256c1f25fafab2c614dcec436fff8efb025991bc
Nathan Marley <nathan.marley@gmail.com>
2017-07-01 23:40:49 -0700

    STI self-referential HABTM association

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,9 @@
 class Team < Entity
+  has_and_belongs_to_many :users, join_table: :teams_users
+
+  #has_many :teams_users, class_name: 'Entity'
+  #has_many :users, class_name: 'Entity', through: :teams_users, source: :users
+
+  # default repo permission
+  # admin write read none
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < Entity
+  has_and_belongs_to_many :teams, join_table: 'teams_users'
 end
