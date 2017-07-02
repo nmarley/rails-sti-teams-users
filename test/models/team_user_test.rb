9afb869087cdc8227a1c9af83a892900812d8a20
Nathan Marley <nathan.marley@gmail.com>
2017-07-02 00:42:17 -0700

    change fixture name

--- test/models/team_user_test.rb
+++ test/models/team_user_test.rb
@@ -5,7 +5,7 @@ require 'test_helper'
 class TeamUserTest < ActiveSupport::TestCase
   test "should not save a team_user without a role" do
     user = users(:nate)
-    team = teams(:team)
+    team = teams(:avengers)
 
     tu = TeamUser.new(user_id: user.id, team_id: team.id)
     assert_not tu.save, "Saved the team_user link without a team role specified"
@@ -13,7 +13,7 @@ class TeamUserTest < ActiveSupport::TestCase
 
   test "should not save a team_user with an invalid role" do
     user = users(:nate)
-    team = teams(:team)
+    team = teams(:avengers)
 
     tu = TeamUser.new(user_id: user.id, team_id: team.id)
     tu.role = 'emperor_for_life'
@@ -22,7 +22,7 @@ class TeamUserTest < ActiveSupport::TestCase
 
   test "should save a team_user with a valid role" do
     user = users(:nate)
-    team = teams(:team)
+    team = teams(:avengers)
 
     tu = TeamUser.new(user_id: user.id, team_id: team.id)
     tu.role = 'owner'
