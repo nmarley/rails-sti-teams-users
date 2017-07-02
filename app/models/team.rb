daac2311d4c71e635c36cafc7893e2382ef41c91
Nathan Marley <nathan.marley@gmail.com>
2017-07-01 23:57:35 -0700

    add validations for team also

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,10 @@
 class Team < Entity
   has_and_belongs_to_many :users, join_table: :teams_users
 
+  # not sure if needed for Team...
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :login, presence: true, uniqueness: { case_sensitive: false }
+
   #has_many :teams_users, class_name: 'Entity'
   #has_many :users, class_name: 'Entity', through: :teams_users, source: :users
 
