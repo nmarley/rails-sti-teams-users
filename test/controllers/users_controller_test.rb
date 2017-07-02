b085db7fe50cb6e64585a2d5ba1f90732330c5ae
Nathan Marley <nathan.marley@gmail.com>
2017-07-02 01:03:31 -0700

    use right name for fixture in controller test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @user = users(:one)
+    @user = users(:nate)
   end
 
   test "should get index" do
